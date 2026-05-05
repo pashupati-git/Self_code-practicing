86f8ea1ebabffd7677f14bcac3aa1c6f52960a05
pashupati-git <pashupatic.github@gmail.com>
2026-05-05 15:04:39 +0545

    basic project structure with home_screen is completed

---
+++ lib/components/bottom_nav_bar.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatelessWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 56,
+      color: Colors.deepPurple,
+    );
+  }
+}
